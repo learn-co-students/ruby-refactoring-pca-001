39121fab351c09ed69eb659eff4801643f37c4c1
Ian Miller <irmiller22@gmail.com>
2014-10-08 18:05:01 -0400

    solution in progress

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,53 +1,59 @@
 class Dog
-  attr_accessor :leash, :plastic_bag, :walking, :vet_checkup
+	attr_accessor :leash, :plastic_bag, :walking, :vet_checkup
 
-  def initialize(name, breed, owner_name)
-    @name = name
-    @breed = breed
-    @owner = owner_name
-    @leash = false
-    @plastic_bag = false
-    @walking = false
-    @vet_checkup = false
-  end
+	def initialize(name, breed, owner_name)
+		@name = name
+		@breed = breed
+		@leash = false
+		@plastic_bag = false
+		@walking = false
+		@vet_checkup = false
+		@owner = Owner.new(owner_name, self)
+	end
 
-  def name
-    @name
-  end
+	def name
+		@name
+	end
 
-  def bark
-    "Woof!"
-  end
+	def bark
+		"Woof!"
+	end
 
-  def breed
-    @breed
-  end
+	def breed
+		@breed
+	end
 
-  def owner
-    @owner
-  end
+	def owner
+		@owner
+	end
 
-  def walking?(owner)
-    owner.dog.walking
-  end
+	def walking?(owner)
+		owner.dog.walking
+	end
 
-  def vet_checkup?(owner)
-    owner.dog.vet_checkup
-  end
+	def vet_checkup?(owner)
+		owner.dog.vet_checkup
+	end
 
-  # Refactor the following methods as methods in the Owner class!
+	def walk(owner)
+		owner.dog.leash = true
+		owner.dog.plastic_bag = true
+		owner.dog.walking = true
+	end
 
-  def walk(owner)
-    # REFACTOR!
-    owner.dog.leash = true
-    owner.dog.plastic_bag = true
-    owner.dog.walking = true
-  end
+	def vet_checkup(owner)
+		owner.dog.leash = true
+		owner.dog.plastic_bag = true
+		owner.dog.vet_checkup = true
+	end
 
-  def vet_checkup(owner)
-    # REFACTOR!
-    owner.dog.leash = true
-    owner.dog.plastic_bag = true
-    owner.dog.vet_checkup = true
-  end
+	def return_home(owner)
+		owner.dog.leash = false
+		owner.dog.plastic_bag = false
+		if owner.dog.walking
+			owner.dog.walking = false
+		else
+			owner.dog.vet_checkup = false
+		end
+	end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
-  # code your solution here!
+	attr_accessor :name, :dog
+
+	def initialize(name, dog)
+		@name = name
+		@dog = dog
+	end
+
+	def walk(dog)
+		prepare_items_for(dog)
+		dog.walking = true
+	end
+
+	def prepare_items_for(dog)
+		dog.leash = true
+		dog.plastic_bag = true
+	end
 end
