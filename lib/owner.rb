9b03f6678031b79ac6d3ac6749894030cc882571
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-11 11:25:58 -0400

    lab complete

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
-  # code your solution here!
+  attr_accessor :name, :dog
+
+  def prepare_items
+    @dog.leash = true
+    @dog.plastic_bag = true
+  end
+
+  def walk
+    prepare_items
+    @dog.walking = true
+  end
+
+  def vet_checkup
+    prepare_items
+    @dog.vet = true
+  end
 end
